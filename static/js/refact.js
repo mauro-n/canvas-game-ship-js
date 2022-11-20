71370cd8abcc739fb2aaf1b31ac8f43307534b68
mauro dev <mauro.henrique613@gmail.com>
2022-11-20 18:49:00 -0300

    complete refac on main logic and classes layout, union of all files into one

--- static/js/refact.js
+++ static/js/refact.js
@@ -5,16 +5,15 @@ class Ship {
         image.src = '../static/images/ship23x34.png';
 
         image.onload = () => {
-            const scaling = 1.25;
             this.img = image;
-            this.width = image.width * scaling;
-            this.height = image.height * scaling;
-            this.position = {
-                x: canvas.width / 2 - this.width,
-                y: canvas.height - this.height * 2,
-            }
         }
-
+        const scaling = 1.25;
+        this.width = image.width * scaling;
+        this.height = image.height * scaling;
+        this.position = {
+            x: canvas.width / 2 - this.width,
+            y: canvas.height - this.height * 2,
+        }
         this.isShooting = true;
     };
 
@@ -34,7 +33,7 @@ class Ship {
 
     draw() {
         ctx.fillRect(0, 0, canvas.width, canvas.height)
-        if (!this.img) { return /* console.log('carregando ship.png...') */};
+        if (!this.img) { return /* console.log('carregando ship.png...') */ };
         ctx.drawImage(
             this.img,
             this.position.x,
@@ -46,7 +45,7 @@ class Ship {
 
 //class enemy
 class Enemy {
-    constructor(qtd = 0, x = 1) {
+    constructor(qtd = 0, skill = [], x = 1 ) {
         const image = document.createElement('img');
         image.src = '../static/images/gift.png';
         image.onload = () => {
@@ -62,73 +61,130 @@ class Enemy {
             y: 50,
         }
 
-        this.texto = [];
-        this.size = [];
-        this.enemies = [];
+        this.skill = skill;
         this.beenHit = false;
         this.qtd = qtd;
     }
 
-    populate() {
+    populate(skills) {
         const qtdPerStage = canvas.width / this.qtd;
 
         for (let i = 0; i < this.qtd; i++) {
             let xPos = (i * qtdPerStage) + (qtdPerStage / 2);//useful formula for evenly spacing
-
             xPos -= this.width / 2 //centers
-
-            const ene = new Enemy(0, xPos);
-
-            this.enemies.push(ene);
+            const ene = new Enemy(0, skills.shift(), xPos);
+            enemies.push(ene);
         }
     }
 
     draw() {
         if (!this.img) { return /* console.log('carregando gift.png..') */ }
 
-        this.enemies.forEach(element => {
+        enemies.forEach(element => {
             if (element.beenHit == true) { return };
             ctx.drawImage(this.img, element.position.x, element.position.y, this.width, this.height);
         })
     }
+
+    death(){
+        if (!this.beenHit){ return };
+        const tombstone = new Tomb(this.skill,
+            this.position, this.width, this.height);
+        deadEnemies.push(tombstone)
+    }
 }
 
+//function for adding images onto  document
+function addImg (icons){
+    icons.forEach( (element) => {
+        const tempImg = document.createElement('img');
+        tempImg.src = `../static/images/${element}.png`;
+        habilidades.push(tempImg);
+    })
+
+}
+
+//class Tomb
+class Tomb {
+    constructor(img, position, w, h){
+        this.img = img;
+        this.position = position;
+        this.width = w;
+        this.height = h;
+    }
+
+    draw(){
+        if (!this.img){ return };
+        if (this.img.length == 0){ return }
+        ctx.drawImage(this.img,
+            this.position.x, 
+            this.position.y,
+            this.width,
+            this.height);
+    }
+
+
+}
+
+//class projectile bullet
 class Bullet {
-    constructor(){
+    constructor(obj) {
         const image = document.createElement('img');
         image.src = '../static/images/rocket.png';
 
-        this.img = img;
-        this.x = x;
-        this.y = y;
-        this.size = 20;
+        image.onload = () => {
+            this.img = image;
+        }
+
         this.speed = 8;
+
+        const scaling = 1.5;
+        this.width = image.width * scaling;
+        this.height = image.height * scaling;
+
+        this.position = {
+            x: obj.x,
+            y: obj.y
+        }
     };
 
-    draw(ctx){
-        ctx.drawImage(this.img, this.x + this.size/2,this.y,this.size,this.size);
+    draw() {
+        if (!this.img) { return }
+        ctx.drawImage(this.img, this.position.x, this.position.y, this.width, this.height);
     };
 
-    move(){
-        this.y -= this.speed
-        if (this.y < 1){data.bullets.shift()}
+    move() {
+        this.position.y -= this.speed
+        if (this.position.y < 0) {
+            setTimeout(() => {
+                bullets.shift();
+            }, 0)
+            //console.log(bullets.length);
+        }
     };
 
-    boxCollision(obj1, obj2){
+    checkCollision(obj1, obj2) { //this is box collision algorithm
         if (
-            obj1.x + obj1.size >= obj2.x &&
-            obj1.x <= obj2.x + obj2.size &&
-            obj1.y + obj1.size >= obj2.y &&
-            obj1.y <= obj2.y + obj2.size
-        ) {return true}
+            obj1.position.x + obj1.width >= obj2.position.x &&
+            obj1.position.x <= obj2.position.x + obj2.width &&
+            obj1.position.y + obj1.height >= obj2.position.y &&
+            obj1.position.y <= obj2.position.y + obj2.height
+        ) { return true }
     }
-    
+
 };
 
 //setting game assets
-const palavras =  ['test1', 'teste2', 'aaaa'];
+
+
+const habilidades = [];
 const bullets = [];
 const enemies = [];
+addImg(['django-logo-icon', 'email-round-icon',
+        'express-js-icon', 'icons8-javascript-48',
+        'mongodb-icon', 'node-js-icon', 'python-icon',
+        'react-js-icon']);
+const deadEnemies = [];
 
 //setting up canvas
 let raf;
@@ -139,16 +195,22 @@ const ctx = canvas.getContext('2d');
 
 //adding ship&&enemy
 const ship = new Ship;
-const enemy = new Enemy(5);
-enemy.populate()
+const enemy = new Enemy(habilidades.length);
+enemy.populate(habilidades);
 
 //adding events
 canvas.addEventListener('click', (e) => {
     if (e.clientX > canvas.width / 2) { ship.moveRight(canvas) }
     if (e.clientX < canvas.width / 2) { ship.moveLeft(canvas) }
-    const bullet = new Bullet(ship.x, ship.y, bulletImg)
+    const bullet = new Bullet(ship.position)
     bullets.push(bullet);
+
+    //console.log(deadEnemies)
 });
+
+/* enemies.forEach(element => {
+    console.log(element.skill)
+}) */
 
 function animate() {
     //clear
@@ -157,6 +219,26 @@ function animate() {
 
     ship.draw()
     enemy.draw()
+
+    //shooting&&collision
+    if (bullets.length > 0){
+        bullets.forEach((el) => {
+            el.draw()
+            el.move()
+            enemies.forEach((el2) => {
+                if (el2.beenHit == true){ return };
+                if (el.checkCollision(el, el2)) {el2.beenHit = true};
+                el2.death();
+            })
+        })
+    }
+
+    //habilidades
+    if (deadEnemies.length > 0){
+        deadEnemies.forEach(element => {
+            element.draw()
+        })
+    }
 
     raf = requestAnimationFrame(animate)
 }
